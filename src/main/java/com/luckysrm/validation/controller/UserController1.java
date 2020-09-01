b2045d6351b7198e77f3d7edde1502e72e33d09a
zhihui_wu01@163.com <1449553037@qq.com>
2020-09-01 15:17:25 +0800

    FEAT 校验方法返回值

--- src/main/java/com/luckysrm/validation/controller/UserController1.java
+++ src/main/java/com/luckysrm/validation/controller/UserController1.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * @Description: ‘@Validated’ 注解于类上
@@ -32,6 +33,25 @@ public class UserController1 {
     public String registerMulty(@NotNull @Min(18) Integer param) {
         System.out.println(param);
         return "success";
+    }
+
+    /**
+     * 校验返回值1
+     */
+    @RequestMapping("/varifyReturn1")
+    @NotNull
+    public String varifyReturn1(String param) {
+        System.out.println(param);
+        return param;
+    }
+
+    /**
+     * 校验返回值2
+     */
+    @RequestMapping("/varifyReturn2")
+    public @NotNull @Size(min = 5) String varifyReturn2(String param) {
+        System.out.println(param);
+        return param;
     }
 
 }
